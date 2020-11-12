4627ed0565cd2f4c65e57f36cccb51a0b2895276
José A. Maita F <jose_a_maita@hotmail.com>
2020-11-12 16:43:28 -0400

    Implement second entry point of web service
     This adds:
    
     - A way to get the "id" of the task in the URL.
     - Entry point with the "get_task(task_id)" function in
     /todo/api/v1.0/tasks/<int:task_id> URI, and only for
     the GET HTTP method.
     - Response of this function with JSON data (jsonify).
     - Command to test:
     .\curl -i http://localhost:5000/todo/api/v1.0/tasks/N
     where N is a number (1, 2, 3, ...).

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, abort
 
 tasks = [
     {
@@ -19,3 +19,10 @@ tasks = [
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
