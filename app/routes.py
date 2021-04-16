ae668cfdf26718371da7aaeacac07efc9f9123bf
José A. Maita F <jose_a_maita@hotmail.com>
2021-04-16 16:53:22 -0400

    Improve web service interface for "update_task(task_id)"
    This adds:
    
    - New "jsonify" response when using "update_task(task_id)" method.

--- app/routes.py
+++ app/routes.py
@@ -56,7 +56,7 @@ def update_task(task_id):
     task[0]['title'] = request.json.get('title', task[0]['title'])
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+    return jsonify({'task': make_public_task(task[0])})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
