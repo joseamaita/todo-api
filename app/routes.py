e6787f860477528a14277dc4364eee68f90d0b08
José A. Maita F <jose_a_maita@hotmail.com>
2021-04-16 11:51:11 -0400

    Insert a new item in our task database (add new task)
    This adds:
    
    - New route "create_task()" with POST method.
    - Command to test:
      ./curl -i -H "Content-Type: application/json" -X POST ...
      ... -d '{"title":"Read a book"}' ...
      ... http://localhost:5000/todo/api/v1.0/tasks

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import jsonify, abort, make_response
+from flask import jsonify, abort, make_response, request
 
 tasks = [
     {
@@ -26,6 +26,19 @@ def get_task(task_id):
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
 
 @app.errorhandler(404)
 def not_found(error):
